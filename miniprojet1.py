fd0f1f9c2589619bdd974b945baa2cc56068b7f4
zakaria izouaouen <15352294+zkrzn@users.noreply.github.com>
2022-11-07 22:27:33 +0100

    Update miniprojet1.py

--- miniprojet1.py
+++ miniprojet1.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title('Mini Projet 1 - PYTHON')
 
 #data = st.file_uploader('Upload a XSL')
-xl_file = 'mini_projet_1.xlsx'
+xl_file = 'https://github.com/zkrzn/Miniprojet1/raw/main/mini_projet_1.xlsx'
 
 
 # Preparation des dataframes
